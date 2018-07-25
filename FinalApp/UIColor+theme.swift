cdb9ef22ad22d70abbc57e7806cc1394430ba1ed
jemtca <30645648+jemtca@users.noreply.github.com>
2018-07-24 23:26:36 -0700

    iOS Final App

---
+++ FinalApp/UIColor+theme.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+theme.swift
+//  FinalApp
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let lightRed = UIColor(red: 247/255, green: 66/255, blue: 82/255, alpha: 1)
+    
+    static let mercury = UIColor(red:  235/255, green: 235/255, blue: 235/255, alpha: 1)
+    
+}
--- FinalApp/ViewController.swift
+++ FinalApp/ViewController.swift
@@ -5,7 +5,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -14,6 +14,15 @@ class ViewController: UIViewController {
         view.backgroundColor = UIColor.white
         
         navigationItem.title = "Business Cards"
+        
+        tableView.backgroundColor = UIColor.mercury
+        // hiding the separators
+        //tableView.separatorStyle = UITableViewCellSeparatorStyle.none
+        tableView.separatorColor = UIColor.black
+        // it doesn't show the separators outside of the table view
+        tableView.tableFooterView = UIView() // blank UIView
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellID")
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "plus").withRenderingMode(UIImageRenderingMode.alwaysOriginal), style: UIBarButtonItemStyle.plain, target: self, action: #selector(handleAddBC))
         
@@ -24,9 +33,38 @@ class ViewController: UIViewController {
         print("Adding BC...")
     }
     
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = UIColor.lightRed
+        return view
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath)
+        
+        cell.backgroundColor = UIColor.white
+        
+        cell.textLabel?.text = "Business card"
+        //cell.textLabel?.textColor = UIColor.mercury
+        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    // function to setup the navigation controller
     func setupNavigationController() {
         navigationController?.navigationBar.isTranslucent = false
         navigationController?.navigationBar.barTintColor = UIColor.black
+        
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor:UIColor.white]
         
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedStringKey.foregroundColor:UIColor.white]
